5786e5a87d9030dcb027c4740feef191fdd29c9b
Neil de Carteret <git@lumphammer.net>
2026-03-05 10:18:25 +0000

    tweak to backfill

--- migrations/chatDB/20260305095912_lively_goblin_queen/migration.sql
+++ migrations/chatDB/20260305095912_lively_goblin_queen/migration.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `users` ADD `chat_id` text NOT NULL;--> statement-breakpoint
 PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `__new_users` (
 	`id` text PRIMARY KEY,
@@ -11,7 +10,7 @@ CREATE TABLE `__new_users` (
 	`chat_id` text NOT NULL UNIQUE
 );
 --> statement-breakpoint
-INSERT INTO `__new_users`(`id`, `name`, `email`, `email_verified`, `image`, `created_at`, `updated_at`) SELECT `id`, `name`, `email`, `email_verified`, `image`, `created_at`, `updated_at` FROM `users`;--> statement-breakpoint
+INSERT INTO `__new_users`(`id`, `name`, `email`, `email_verified`, `image`, `created_at`, `updated_at`, `chat_id`) SELECT `id`, `name`, `email`, `email_verified`, `image`, `created_at`, `updated_at`, `id` FROM `users`;--> statement-breakpoint
 DROP TABLE `users`;--> statement-breakpoint
 ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;
