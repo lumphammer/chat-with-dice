105b04bef44430e19151613865a5688f6515e75f
Neil de Carteret <git@lumphammer.net>
2026-03-21 15:24:20 +0000

    improve havoc hat form

--- src/rollTypes/havoc/HavocComponents.tsx
+++ src/rollTypes/havoc/HavocComponents.tsx
@@ -1,15 +1,29 @@
 import type { HavocFormula, HavocResult } from "./havocValidators";
-import { memo } from "react";
+import styles from "@/styles/inputs.module.css";
+import { memo, useId } from "react";
 
 export const HavocInputUI = memo(
   ({ onChange }: { onChange: (formula: HavocFormula) => void }) => {
+    // useEffect(() => {}, []);
+    const id = useId();
+
     return (
-      <input
-        type="number"
-        onChange={(e) => {
-          onChange({ numDice: parseInt(e.target.value, 10) });
-        }}
-      />
+      <>
+        <label className={`${styles.label} w-max-20`} htmlFor={id}>
+          Number of dice:
+        </label>
+        <input
+          id={id}
+          type="number"
+          min={1}
+          defaultValue={1}
+          className={`${styles.input} max-w-40 flex-1 px-4 pr-0 text-left`}
+          placeholder="Number of dice"
+          onChange={(e) => {
+            onChange({ numDice: parseInt(e.target.value, 10) });
+          }}
+        />
+      </>
     );
   },
 );
