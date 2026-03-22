625479cc9c03d6fcfba3b00a8cc7729494f45117
Neil de Carteret <git@lumphammer.net>
2026-03-22 18:45:27 +0000

    [wip]

--- src/lib/reducerTools.ts
+++ src/lib/reducerTools.ts
@@ -7,6 +7,7 @@ import {
   useRef,
   useState,
 } from "react";
+import type { z } from "zod";
 
 /**
  * A minimal case for all actions - there will always be a `type` and a
@@ -28,14 +29,17 @@ export type AnyAction = {
  *  * an `apply` function, which take a state and an action and applies the
  *    relevant reducer if the action matches the case
  */
-function createCase<S, P = void>(
+function createCase<TState, TPayloadValidator extends z.ZodAny>(
   type: string,
-  reducer: (draft: S, payload: P) => void,
+  payloadValidator: TPayloadValidator,
+  reducer: (draft: TState, payload: z.infer<TPayloadValidator>) => void,
 ) {
-  const create = (payload: P) => ({ type, payload });
-  const match = (action: AnyAction): action is { type: string; payload: P } =>
+  const create = (payload: z.infer<TPayloadValidator>) => ({ type, payload });
+  const match = (
+    action: AnyAction,
+  ): action is { type: string; payload: z.infer<TPayloadValidator> } =>
     action.type === type;
-  const apply = (state: S, action: AnyAction) => {
+  const apply = (state: TState, action: AnyAction) => {
     if (match(action)) {
       return reducer ? reducer(state, action.payload) : state;
     }
@@ -52,12 +56,19 @@ function createCase<S, P = void>(
   };
 }
 
-type Reducers<S> = { [key: string]: (state: Draft<S>, payload?: any) => void };
-
-export type CreateSliceArgs = {
-  onError?: (error: unknown) => void;
-  log?: (...args: unknown[]) => void;
+type ReducerObject<TState, TPayloadValidator extends z.ZodAny = z.ZodAny> = {
+  payloadValidator: TPayloadValidator;
+  reducer: (state: Draft<TState>, payload: z.infer<TPayloadValidator>) => void;
 };
+
+type Reducers<TState> = {
+  [key: string]: ReducerObject<TState>;
+};
+
+// export type CreateSliceArgs = {
+//   onError?: (error: unknown) => void;
+//   log?: (...args: unknown[]) => void;
+// };
 
 /**
  * A minimal reimagination of the `createSlice` function from
@@ -65,67 +76,69 @@ export type CreateSliceArgs = {
  * This function is curried so that we can specify S (state) but allow R
  * (reducers) to be inferred.
  */
-export const createSlice =
-  <S extends object>(args: CreateSliceArgs = {}) =>
-  <R extends Reducers<S>>(reducers: R) => {
-    // turn all the reducers into cases (so they can be created and applied)
-    const sliceCases = Object.entries(reducers).map(([key, reducer]) => {
-      const action = createCase(key, reducer);
+export const createSlice = <
+  TValidator extends z.ZodTypeAny,
+  TReducers extends Reducers<z.infer<TValidator>>,
+>(
+  validator: TValidator,
+  reducers: TReducers,
+) => {
+  // turn all the reducers into cases (so they can be created and applied)
+  const sliceCases = Object.entries(reducers).map(
+    ([key, { payloadValidator, reducer }]) => {
+      const action = createCase(key, payloadValidator, reducer);
       return [key, action] as const;
-    });
-    // turn those cases back into an object of creators.
-    const creators = Object.fromEntries(
-      sliceCases.map(([key, action]) => [key, action.create]),
-    ) as {
-      // we need this mad cast because `map` can't give us accurate types.
-      // we're just knitting together the `typeof createCase<...>["create"]`
-      // and working out the payload type using inference.
-      [key in keyof R]: ReturnType<
-        typeof createCase<
-          S,
-          R[key] extends (state: infer S1) => void
-            ? void
-            : R[key] extends (state: infer S1, payload: infer P1) => void
-              ? P1
-              : never
-        >
-      >["create"];
-    };
-    // `useDispatch` takes the initial state as an argument, so we don't need to
-    // provide a default argument for `state` here (unlike e.g. a Redux
-    // reducer).
-    const reducer = (state: S, action: AnyAction) => {
-      try {
-        const newState = produce(state, (draft) => {
-          for (const [, sliceCase] of sliceCases) {
-            sliceCase.apply(draft, action);
-          }
-        });
-        if (args.log) {
-          args.log(action, state, newState);
-        }
-        return newState;
-      } catch (e) {
-        // in the event of an error, we will try to keep going rather than just
-        // exploding
-        if (args.onError) {
-          args.onError(e);
-        }
-        return state;
-      }
-    };
-
-    return {
-      /**
-       * A dictionary of action creators, keyed by the name of the case.
-       */
-      creators,
-      /**
-       * The reducer function for this slice.
-       */
-      reducer,
-    };
+    },
+  );
+  // turn those cases back into an object of creators.
+  const creators = Object.fromEntries(
+    sliceCases.map(([key, action]) => [key, action.create]),
+  ) as {
+    // we need this mad cast because `map` can't give us accurate types.
+    // we're just knitting together the `typeof createCase<...>["create"]`
+    // and working out the payload type using inference.
+    [key in keyof TReducers]: ReturnType<
+      typeof createCase<
+        S,
+        TReducers[key] extends (state: unknown) => void
+          ? void
+          : TReducers[key] extends (state: unknown, payload: infer P1) => void
+            ? P1
+            : never
+      >
+    >["create"];
   };
+  // `useDispatch` takes the initial state as an argument, so we don't need to
+  // provide a default argument for `state` here (unlike e.g. a Redux
+  // reducer).
+  const reducer = (state: S, action: AnyAction) => {
+    try {
+      const newState = produce(state, (draft) => {
+        for (const [, sliceCase] of sliceCases) {
+          sliceCase.apply(draft, action);
+        }
+      });
+
+      return newState;
+    } catch (e) {
+      // in the event of an error, we will try to keep going rather than just
+      // exploding
+      console.error(e);
+      return state;
+    }
+  };
+
+  return {
+    /**
+     * A dictionary of action creators, keyed by the name of the case.
+     */
+    creators,
+    /**
+     * The reducer function for this slice.
+     */
+    reducer,
+  };
+};
 
 /**
  * Create a `useSelector` hook that uses the given `context`.
