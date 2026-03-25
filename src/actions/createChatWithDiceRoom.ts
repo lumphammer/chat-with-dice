197d82227d3668f610be9dc0a3e5e852d3d8e9ae
Neil de Carteret <git@lumphammer.net>
2026-03-25 15:34:51 +0000

    include do id and config when creating a room

--- src/actions/createChatWithDiceRoom.ts
+++ src/actions/createChatWithDiceRoom.ts
@@ -1,6 +1,7 @@
 import { ROOM_TYPES } from "#/constants";
 import { db } from "#/db";
 import { Rooms } from "#/schemas/chatDB-schema";
+import { defaultRoomConfig } from "#/workers/DiceRollerRoom/defaultRoomConfig";
 import { z } from "astro/zod";
 import { defineAction } from "astro:actions";
 import { env } from "cloudflare:workers";
@@ -23,14 +24,24 @@ export const createChatWithDiceRoom = defineAction({
     const RollerNamespace = env.DiceRollerRoom;
     if (!RollerNamespace)
       return new Response("Roller binding not found", { status: 500 });
+
     const roomId = nanoid();
+
+    // Get a Durable Object ID for this room
+    // idFromName ensures the same room ID always maps to the same Durable Object
+    const durableObjectId = RollerNamespace.idFromName(roomId).toString();
+
+    // we *could* use the DO id as the pk, but that feels leaky
+
     await db.insert(Rooms).values({
       created_by_user_id: user.id,
       created_time: Date.now(),
       name: input.roomName,
       description: input.description,
-      id: roomId,
+      id: durableObjectId,
       type: input.type,
+      config: JSON.stringify(defaultRoomConfig),
+      durableObjectId,
     });
     return { roomId };
   },
