80a2e4d1bfe59815923d996c59e817cdcbe94683
Neil de Carteret <git@lumphammer.net>
2026-04-06 00:01:03 +0100

    tidy up declaration of counterCapability

--- src/capabilities/counterCapability.ts
+++ src/capabilities/counterCapability.ts
@@ -1,25 +1,21 @@
 import { createCapability } from "./createCapability";
 import { z } from "zod/v4";
 
-const counterCapabilityStateValidator = z.object({ count: z.int() });
-
 export const counterCapability = createCapability({
   name: "counter",
   configValidator: z.object({ startAt: z.int() }),
   defaultConfig: { startAt: 0 },
-  stateValidator: counterCapabilityStateValidator,
+  stateValidator: z.object({ count: z.int() }),
   getInitialState: ({ config }) => ({ count: config.startAt }),
   initialise: async () => {},
   buildActions: ({ createAction }) => {
-    const increment = createAction({
-      payloadValidator: z.object({ by: z.number() }),
-      pureFn: async ({ stateDraft, payload }) => {
-        stateDraft.count += payload.by;
-      },
-    });
-
     return {
-      increment,
+      increment: createAction({
+        payloadValidator: z.object({ by: z.number() }),
+        pureFn: async ({ stateDraft, payload }) => {
+          stateDraft.count += payload.by;
+        },
+      }),
     };
   },
 });
