d03f8047cedcb09a4703cb832197a294f68fedc0
Neil de Carteret <git@lumphammer.net>
2026-04-06 00:03:38 +0100

    add ability to get individual messages from message repository

--- src/workers/DiceRollerRoom/MessageRepository.ts
+++ src/workers/DiceRollerRoom/MessageRepository.ts
@@ -1,12 +1,22 @@
 import * as dbSchema from "#/schemas/roller-schema";
 import type { RollerMessage } from "#/validators/rollerMessageType";
-import { desc } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import type { DrizzleSqliteDODatabase } from "drizzle-orm/durable-sqlite";
 
 const MESSAGE_CATCHUP_LENGTH = 100;
 
 export class MessageRepository {
   constructor(private db: DrizzleSqliteDODatabase<typeof dbSchema>) {}
+
+  async getById(id: string): Promise<RollerMessage | undefined> {
+    return (
+      await this.db
+        .select()
+        .from(dbSchema.Messages)
+        .where(eq(dbSchema.Messages.id, id))
+        .execute()
+    ).at(0);
+  }
 
   async insert(message: RollerMessage): Promise<void> {
     await this.db.insert(dbSchema.Messages).values(message);
