cd632e7340ddfef788ca51d7ea37e41c3bb1a8d9
Neil de Carteret <git@lumphammer.net>
2026-04-11 10:45:58 +0100

    better tabs

--- src/components/DiceRoller/Sidebar.tsx
+++ src/components/DiceRoller/Sidebar.tsx
@@ -9,17 +9,26 @@ import { memo } from "react";
 export const Sidebar = memo(({ config }: { config: RoomConfig }) => {
   return (
     <Tabs.Root
-      className="bg-base-100 w-sm overflow-hidden"
+      className="bg-base-100 relative w-sm"
       defaultValue={config.capabilities[0]?.name}
+      orientation="vertical"
     >
-      <Tabs.List>
+      <Tabs.List className="absolute right-full flex flex-col gap-4">
         {config.capabilities.map(({ name }) => {
           if (!isCapabilityName(name)) {
             return null;
           }
           const Component = capabilityRegistry[name].iconComponent;
           return (
-            <Tabs.Trigger key={name} className="" value={name}>
+            <Tabs.Trigger
+              key={name}
+              className="border-base-content/30 hover:border-accent
+                hover:bg-accent/30 aria-selected:bg-accent text-base-content
+                aria-selected:text-accent-content block rounded border p-1
+                shadow-2xl transition-transform not-aria-selected:cursor-pointer
+                hover:not-aria-selected:scale-120"
+              value={name}
+            >
               <Component />
             </Tabs.Trigger>
           );
