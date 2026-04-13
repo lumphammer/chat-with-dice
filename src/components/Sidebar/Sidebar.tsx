251ebd51036b89b3e51e054e99d3bb9d2da7734a
Neil de Carteret <git@lumphammer.net>
2026-04-13 11:13:26 +0100

    spaceholder in mobile mode

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,72 +11,77 @@ export const Sidebar = memo(({ config }: { config: RoomConfig }) => {
   const ref = useRef<HTMLElement>(null);
 
   return (
-    <Tabs.Root
-      className={styles.root}
-      defaultValue={config.capabilities[0]?.name}
-      orientation="vertical"
-      asChild
-    >
-      <aside ref={ref}>
-        <Tabs.List className={styles.tabList} asChild>
-          <nav>
+    <>
+      <div className={styles.spaceHolder} />
+
+      <Tabs.Root
+        className={styles.root}
+        defaultValue={config.capabilities[0]?.name}
+        orientation="vertical"
+        asChild
+      >
+        <aside ref={ref}>
+          <Tabs.List className={styles.tabList} asChild>
+            <nav>
+              {config.capabilities.map(({ name }) => {
+                if (!isCapabilityName(name)) {
+                  return null;
+                }
+                const Component = capabilityRegistry[name].iconComponent;
+                return (
+                  <Tabs.Trigger
+                    key={name}
+                    className={styles.tabButton}
+                    value={name}
+                    onClick={(e) => {
+                      if (!ref.current) {
+                        return;
+                      }
+                      console.log(e.currentTarget, ref.current);
+                      const isSelected =
+                        e.currentTarget.ariaSelected === "true";
+                      if ("desktopClosed" in ref.current.dataset) {
+                        delete ref.current.dataset.desktopClosed;
+                      } else if (isSelected) {
+                        ref.current.dataset.desktopClosed = "true";
+                      }
+                      if (!("mobileOpen" in ref.current.dataset)) {
+                        ref.current.dataset.mobileOpen = "true";
+                      } else if (isSelected) {
+                        delete ref.current.dataset.mobileOpen;
+                      }
+                    }}
+                  >
+                    <Component />
+                  </Tabs.Trigger>
+                );
+              })}
+            </nav>
+          </Tabs.List>
+          <section className={styles.contentArea}>
             {config.capabilities.map(({ name }) => {
               if (!isCapabilityName(name)) {
                 return null;
               }
-              const Component = capabilityRegistry[name].iconComponent;
+              const Component = capabilityRegistry[name].sidebarComponent;
+
               return (
-                <Tabs.Trigger
-                  key={name}
-                  className={styles.tabButton}
-                  value={name}
-                  onClick={(e) => {
-                    if (!ref.current) {
-                      return;
-                    }
-                    console.log(e.currentTarget, ref.current);
-                    const isSelected = e.currentTarget.ariaSelected === "true";
-                    if ("desktopClosed" in ref.current.dataset) {
-                      delete ref.current.dataset.desktopClosed;
-                    } else if (isSelected) {
-                      ref.current.dataset.desktopClosed = "true";
-                    }
-                    if (!("mobileOpen" in ref.current.dataset)) {
-                      ref.current.dataset.mobileOpen = "true";
-                    } else if (isSelected) {
-                      delete ref.current.dataset.mobileOpen;
-                    }
-                  }}
-                >
-                  <Component />
-                </Tabs.Trigger>
+                Component && (
+                  <Tabs.Content
+                    key={name}
+                    value={name}
+                    className={styles.tabContent}
+                  >
+                    <div className={styles.tabContentInner}>
+                      <Component />
+                    </div>
+                  </Tabs.Content>
+                )
               );
             })}
-          </nav>
-        </Tabs.List>
-        <section className={styles.contentArea}>
-          {config.capabilities.map(({ name }) => {
-            if (!isCapabilityName(name)) {
-              return null;
-            }
-            const Component = capabilityRegistry[name].sidebarComponent;
-
-            return (
-              Component && (
-                <Tabs.Content
-                  key={name}
-                  value={name}
-                  className={styles.tabContent}
-                >
-                  <div className={styles.tabContentInner}>
-                    <Component />
-                  </div>
-                </Tabs.Content>
-              )
-            );
-          })}
-        </section>
-      </aside>
-    </Tabs.Root>
+          </section>
+        </aside>
+      </Tabs.Root>
+    </>
   );
 });
