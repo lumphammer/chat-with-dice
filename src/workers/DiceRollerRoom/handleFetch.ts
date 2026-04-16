958334a49adbb708511a41ff1b473fa8910aa27f
Neil de Carteret <git@lumphammer.net>
2026-04-16 11:27:44 +0100

    instrument fetch requests

--- src/workers/DiceRollerRoom/handleFetch.ts
+++ src/workers/DiceRollerRoom/handleFetch.ts
@@ -2,6 +2,7 @@ import type { ServerMountedCapability } from "#/capabilities/types";
 import type { Broadcaster } from "./Broadcaster";
 import type { MessageRepository } from "./MessageRepository";
 import type { SessionAttachment } from "./types";
+import { log, logError } from "./utils";
 
 const CATCHUP_DELAY_MS = 100;
 
@@ -14,14 +15,22 @@ export async function handleFetch(
 ) {
   const upgradeHeader = request.headers.get("Upgrade");
   if (upgradeHeader !== "websocket") {
+    logError("Expected WebSocket upgrade");
     return new Response("Expected WebSocket upgrade", { status: 426 });
   }
   const chatId = URL.parse(request.url)?.searchParams.get("chatId");
   if (!chatId) {
+    logError("chatId was falsy");
     return new Response("chatId is required", { status: 400 });
   }
 
   const userId = URL.parse(request.url)?.searchParams.get("userId") ?? null;
+  if (!userId) {
+    logError("userId was falsy");
+    return new Response("userId is required", { status: 400 });
+  }
+
+  log("Accepting WS fetch request, chatId: ", chatId, "userId: ", userId);
 
   // Create a WebSocket pair (client and server)
   const pair = new WebSocketPair();
@@ -35,12 +44,16 @@ export async function handleFetch(
   const attachment: SessionAttachment = { chatId, userId };
   server.serializeAttachment(attachment);
 
+  log("created WS attachment", attachment);
   // this is lame, but FF dev tools fails to show ws messages sent immediately
   // when the socket is opened
   // https://bugzilla.mozilla.org/show_bug.cgi?id=1719394
   setTimeout(async () => {
-    broadcaster.sendCatchUp(server, await messageRepository.getRecent());
+    const messages = await messageRepository.getRecent();
+    log("Sending catchup message count", messages.length);
+    broadcaster.sendCatchUp(server, messages);
     for (const capability of capabilities.values()) {
+      log("sending init for", capability.name);
       broadcaster.sendCapabilityInit(server, capability);
     }
   }, CATCHUP_DELAY_MS);
