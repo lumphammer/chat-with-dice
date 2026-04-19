8218859843b556346452d632da9ef7678ff025eb
Neil de Carteret <git@lumphammer.net>
2026-04-19 18:10:32 +0100

    take control of theme application

---
+++ migrations/chatDB/20260419162104_amused_natasha_romanoff/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `rooms` ADD `theme` text;
--- src/schemas/chatDB-schema.ts
+++ src/schemas/chatDB-schema.ts
@@ -9,10 +9,9 @@ export const rooms = sqliteTable("rooms", {
   description: text(),
   created_by_user_id: text().notNull(),
   created_time: int().notNull(),
-  // need a wee cast here to persuade the drizzle types that the liost of
-  // ROOM_TYPE_NAMES is non-empty
   type: text({ enum: ROOM_TYPE_NAMES }).notNull(),
   config: text({ mode: "json" }),
   durableObjectId: text(),
   deleted_time: int(),
+  theme: text(),
 });
