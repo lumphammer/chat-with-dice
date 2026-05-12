692231299c552243396076f4f4dc1557151234ed
Neil de Carteret <git@lumphammer.net>
2026-05-12 10:32:47 +0100

    clean up do fetching for files

--- src/pages/api/files/[nodeId].ts
+++ src/pages/api/files/[nodeId].ts
@@ -30,11 +30,19 @@ export const GET: APIRoute = async (ctx) => {
 
   const { nodeId } = ctx.params;
   if (!nodeId) {
-    return json({ error: "Missing node ID" }, HTTP_NOT_FOUND);
+    return json({ error: "Missing node ID parameter" }, HTTP_NOT_FOUND);
+  }
+  if (!user.userDataDOId) {
+    return json(
+      { error: "User does not have a durable object id" },
+      HTTP_NOT_FOUND,
+    );
   }
 
   // here
-  const userDataDO = env.USER_DATA_DO.getByName(user.id);
+  const userDataDO = env.USER_DATA_DO.get(
+    env.USER_DATA_DO.idFromString(user.userDataDOId),
+  );
   const node = await userDataDO.getFile(nodeId);
 
   const r2Object = await bucket.get(node.file.r2Key);
