915cf56b439d59dc4ef6bcb6be4ae31b312f3998
Neil de Carteret <git@lumphammer.net>
2026-05-15 19:36:54 +0100

    start work on files sidebar tab

--- src/components/capabilityComponents/Files/SidebarFiles.tsx
+++ src/components/capabilityComponents/Files/SidebarFiles.tsx
@@ -1,6 +1,10 @@
 import { filesCapability } from "#/capabilities/filesCapability";
 import { SidebarPanel } from "#/components/capabilityComponents/shared/SidebarPanel";
+import { FileManager } from "#/pages/files/_components/FileManager";
+import { Tabs } from "@ark-ui/react/tabs";
 import { memo } from "react";
+
+const emptyArray: any[] = [];
 
 export const SidebarFiles = memo(() => {
   const capInfo = filesCapability.useMount();
@@ -11,7 +15,27 @@ export const SidebarFiles = memo(() => {
 
   return (
     <SidebarPanel title="Files" isSaving={capInfo.patches.length > 0}>
-      Files
+      <Tabs.Root
+        className="tabs tabs-border"
+        defaultValue={"shared"}
+        orientation="horizontal"
+        // asChild
+      >
+        <Tabs.List className="tabs tabs-lift w-full">
+          <Tabs.Trigger className="tab aria-selected:tab-active" value="shared">
+            Shared
+          </Tabs.Trigger>
+          <Tabs.Trigger className="tab aria-selected:tab-active" value="mine">
+            Mine
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content className="" value="shared">
+          Shared files
+        </Tabs.Content>
+        <Tabs.Content className="" value="mine">
+          <FileManager initialNodes={emptyArray} />
+        </Tabs.Content>
+      </Tabs.Root>
     </SidebarPanel>
   );
 });
