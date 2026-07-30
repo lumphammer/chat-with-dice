36f005281236c20e6a0631f362f4b9424c1f6b06
Neil de Carteret <git@lumphammer.net>
2026-07-30 13:24:04 +0100

    slight correctness gain

--- src/components/capabilityComponents/SidebarRoll/SidebarRoll.tsx
+++ src/components/capabilityComponents/SidebarRoll/SidebarRoll.tsx
@@ -2,11 +2,21 @@ import { rollClient } from "#/capabilities/roll/client";
 import { useCloseMobileSidebar } from "#/components/Sidebar/mobileSidebarContext";
 import { SidebarPanel } from "#/components/capabilityComponents/shared/SidebarPanel";
 import { RollForm } from "./RollForm";
-import { memo } from "react";
+import { memo, useCallback, type ComponentProps } from "react";
 
 export const SidebarRoll = memo(() => {
   const capInfo = rollClient.useMount();
   const closeMobileSidebar = useCloseMobileSidebar();
+
+  const handleRoll = useCallback(
+    (formula: Parameters<ComponentProps<typeof RollForm>["onRoll"]>[0]) => {
+      if (capInfo.initialised) {
+        capInfo.actions.doRoll(formula);
+        closeMobileSidebar();
+      }
+    },
+    [capInfo, closeMobileSidebar],
+  );
 
   if (!capInfo.initialised) {
     return "Loading...";
@@ -14,12 +24,7 @@ export const SidebarRoll = memo(() => {
 
   return (
     <SidebarPanel title="Roll Dice" isSaving={capInfo.patches.length > 0}>
-      <RollForm
-        onRoll={(formula) => {
-          capInfo.actions.doRoll(formula);
-          closeMobileSidebar();
-        }}
-      />
+      <RollForm onRoll={handleRoll} />
     </SidebarPanel>
   );
 });
